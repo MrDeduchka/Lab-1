ad449936e78cdad21f0416bad3e4ede5324ff31a
Ded <paaAngarsk@yandex.ru>
2019-09-13 22:30:50 +0800

    Revert "Revert "Добавьте файлы проекта.""
    This reverts commit 74739b00b04e205728c317899b4951f700bb958e.

---
+++ Lab-1-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите уравнение 1 ");
+            Console.Write("Введите a: ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите b: ");
+            int b1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите уравнение 2 ");
+            Console.Write("Введите a: ");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите b: ");
+            int b2 = Convert.ToInt32(Console.ReadLine());
+
+            if ((a1 == a2) && (b1 == b2))
+                    { Console.WriteLine("Прямые совпадают"); }
+            else if (a1 == a2)
+                    { Console.WriteLine("Прямые параллельны"); }
+            else
+                    { Console.WriteLine("Прямые пересекаются"); }
+            Console.ReadKey();
+        }
+    }
+}
