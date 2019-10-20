f4af061a81a6fb0730dc02ae4098bfa794b09d2d
pro100 <MrDeduchka@yandex.ru>
2019-10-20 20:41:03 +0700

    Update and rename LAB-1.sln to LAB-3.cs

---
+++ LAB-3.cs
@@ -0,0 +1,19 @@
+﻿
+int sum = 0;
+            int res = 0;
+            int[] M = new int[9];
+            for (int i = 0; i < M.Length; i++)
+            {
+                Console.WriteLine($"Введите число {i+1}:");
+                M[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < M.Length; i++)
+            {
+                if(M[i] % 5 == 0 && M[i] % 7 != 0)
+                {
+                    sum += M[i];
+                    res += 1;
+                }
+            }
+            Console.WriteLine($"Сумма элементов: {sum}. Количество: {res}");
+            Console.ReadKey();
